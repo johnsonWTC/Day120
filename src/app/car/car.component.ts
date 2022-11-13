1ffc8037cbc95293c83302bdaa6f5b40e70e695d
/ohnsonWTC <johnsondubula@gmail.com>
2022-11-13 21:43:20 +0200

    new component

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+
+export interface Car{
+  doorCalor : string,
+  wheelSize : number,
+  carType : string
+  
+}
+
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
