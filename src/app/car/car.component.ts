fd750d6c2ae33bc9be9d8c2b2a87f98b62adb689
/ohnsonWTC <johnsondubula@gmail.com>
2022-11-13 21:50:17 +0200

    Cares

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -20,4 +20,27 @@ export class CarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  cars : Car[] = [
+    {
+      carType: "BMW",
+      doorCalor: "Red",
+      wheelSize: 20
+    }, {
+      carType: "Figo",
+      doorCalor: "blue",
+      wheelSize: 20
+    },
+    {
+      carType: "Polo",
+      doorCalor: "white",
+      wheelSize: 20
+    }
+    , {
+      carType: "Mazda",
+      doorCalor: "Red",
+      wheelSize: 20
+    }
+] 
+
 }
